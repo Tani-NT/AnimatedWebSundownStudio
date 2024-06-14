e4e9c952e776ef61cbd7111c383a3369920b235a
Tani-NT <tanyanithale@gmail.com>
2024-06-14 09:21:54 +0530

    loader added

--- script.js
+++ script.js
@@ -32,19 +32,27 @@ function swiperAnimation(){
 }
 swiperAnimation();
 
-var menu = document.querySelector("nav h3")
-var full = document.querySelector("#full-screen")
-var navimg = document.querySelector("nav img")
-var flag = 0
-menu.addEventListener("click", function(){
-    if(flag == 0){
-        full.style.top = 0
-        navimg.style.opacity = 0
-        flag = 1
-    }
-    else{
-        full.style.top = "-100%"
-        navimg.style.opacity = 1
-        flag = 0
-    }
-})
+function menuAnimation(){
+    var menu = document.querySelector("nav h3")
+    var full = document.querySelector("#full-screen")
+    var navimg = document.querySelector("nav img")
+    var flag = 0
+    menu.addEventListener("click", function(){
+        if(flag == 0){
+            full.style.top = 0
+            navimg.style.opacity = 0
+            flag = 1
+        }
+        else{
+            full.style.top = "-100%"
+            navimg.style.opacity = 1
+            flag = 0
+        }
+    })
+}
+menuAnimation();
+
+var loader = document.querySelector("#loader");
+setTimeout(()=>{
+    loader.style.top = "-100%"
+},4000)
